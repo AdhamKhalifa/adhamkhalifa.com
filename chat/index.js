b2e2b3965cd62e4075330fc36aa96684511cacea
Adham Khalifa <akhalifae@conncoll.edu>
2020-09-03 20:29:27 -0400

    Update index.js

--- chat/index.js
+++ chat/index.js
@@ -1,18 +1,28 @@
-var app = require('express')();
+var express = require('express');
+var app = require('express');
 var http = require('http').createServer(app);
-var io = require('socket.io')(http);
+var server = http.createServer(app);
+var io = require('socket.io').listen(server);
 
-app.get('/', (req, res) => {
+
+server.listen(8888);
+
+app.get('/', function (req, res) {
+  res.sendfile(__dirname + '/index.html');
+});
+
+/*app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
   });
+*/
   
   io.on('connection', (socket) => {
     console.log('a user connected');
   });
   
-  http.listen(3000, () => {
-    console.log('listening on *:3000');
-  });
+  //http.listen(3000, () => {
+    //console.log('listening on *:3000');
+  //});
 
   io.on('connection', (socket) => {
     console.log('a user connected');
