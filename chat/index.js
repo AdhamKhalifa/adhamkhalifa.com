0c8f0cfe5e1547692dbde0b69511e87e32a3dafc
Adham Khalifa <akhalifae@conncoll.edu>
2020-09-10 15:52:43 -0400

    Revert "Update index.js"
    This reverts commit b2e2b3965cd62e4075330fc36aa96684511cacea.

--- chat/index.js
+++ chat/index.js
@@ -1,28 +1,18 @@
-var express = require('express');
-var app = require('express');
+var app = require('express')();
 var http = require('http').createServer(app);
-var server = http.createServer(app);
-var io = require('socket.io').listen(server);
+var io = require('socket.io')(http);
 
-
-server.listen(8888);
-
-app.get('/', function (req, res) {
-  res.sendfile(__dirname + '/index.html');
-});
-
-/*app.get('/', (req, res) => {
+app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
   });
-*/
   
   io.on('connection', (socket) => {
     console.log('a user connected');
   });
   
-  //http.listen(3000, () => {
-    //console.log('listening on *:3000');
-  //});
+  http.listen(3000, () => {
+    console.log('listening on *:3000');
+  });
 
   io.on('connection', (socket) => {
     console.log('a user connected');
