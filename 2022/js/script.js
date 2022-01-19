54ed05bbb91f10df53d0d0c884869770485ff9a7
Adham Khalifa <adham@adhamkhalifa.com>
2022-01-19 04:24:30 -0500

    Experience section
    Half-way

--- 2022/js/script.js
+++ 2022/js/script.js
@@ -43,7 +43,7 @@ TxtType.prototype.tick = function() {
     this.el.innerHTML = '<span class="mid-heading"> I am a '+this.txt+'</span>';
 
     var that = this;
-    var delta = 150 - Math.random() * 100;
+    var delta = 200 - Math.random() * 100;
 
     if (this.isDeleting) { delta /= 2; }
 
@@ -58,7 +58,7 @@ TxtType.prototype.tick = function() {
 
     setTimeout(function() {
     that.tick();
-    }, 100);
+    }, delta);
 };
 
 window.onload = function() {
