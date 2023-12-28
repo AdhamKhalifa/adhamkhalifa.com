01868b705cd0d3b1c3af2f7e12596a4967a121d3
Adham Khalifa <akhalifae@conncoll.edu>
2023-12-28 18:55:01 -0500

    Basic React files added

---
+++ src/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer>
+      {/* Footer content */}
+      <h4>Made with <i className="fas fa-heart"></i> by Adham Khalifa</h4>
+    </footer>
+  );
+}
+
+export default Footer;
